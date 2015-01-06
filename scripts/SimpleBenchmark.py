79d13ed0f94c716c5e79df711618226a02764de4
arnold <akalmbach@gmail.com>
2015-01-06 16:36:40 -0500

    Added a lunchfile

--- scripts/SimpleBenchmark.py
+++ scripts/SimpleBenchmark.py
@@ -5,16 +5,18 @@ import rospy
 from std_msgs.msg import Duration
 from simple_benchmark.msg import StringStamped
 
-PAYLOAD_TIMESTEP = rospy.Duration(10)
+PAYLOAD_TIMESTEP = rospy.Duration(3)
 
 class SimpleBenchmark:
-    def __init__(self, name, othername):
-        self.name = name
-        self.othername = othername
-        rospy.init_node(name)
-        self.dataPub = rospy.Publisher('/'+othername+'/data', StringStamped, queue_size=1)
-        self.otherDataSub = rospy.Subscriber('/'+name+'/data', StringStamped, self.echoMessageCallback)
-        self.delayPub = rospy.Publisher('/'+name+'/delay', Duration, queue_size=1)
+    def __init__(self):
+        rospy.init_node("string_pinger")
+        
+        self.name = rospy.get_param("~name")
+        self.othername = rospy.get_param("~othername")
+        
+        self.dataPub = rospy.Publisher('/'+self.othername+'/data', StringStamped, queue_size=1)
+        self.otherDataSub = rospy.Subscriber('/'+self.name+'/data', StringStamped, self.echoMessageCallback)
+        self.delayPub = rospy.Publisher('/'+self.name+'/delay', Duration, queue_size=1)
         self.lastTime = rospy.Time.now()
         self.lastPayloadIncreaseTime = rospy.Time()
         self.initialized = False
@@ -43,7 +45,7 @@ class SimpleBenchmark:
         if (rospy.Time.now() - self.lastPayloadIncreaseTime > PAYLOAD_TIMESTEP):
             self.lastPayloadIncreaseTime = rospy.Time.now()
             self.payloadSize *= 2
-            print "Increasing payload size to " + str(self.payloadSize)
+            rospy.loginfo("Increasing payload size to " + str(self.payloadSize))
 
         self.echoMessage.data = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(self.payloadSize))
         self.echoMessage.header.stamp = rospy.Time.now()
@@ -51,7 +53,9 @@ class SimpleBenchmark:
         self.delayPub.publish(Duration(delay))
         self.dataPub.publish(self.echoMessage)
         self.lastTime = msg.header.stamp
-        
+
+        if self.payloadSize > 100:
+            rospy.signal_shutdown("finished")
                 
 if __name__ == "__main__":
-    SimpleBenchmark(sys.argv[1], sys.argv[2])
+    SimpleBenchmark()
